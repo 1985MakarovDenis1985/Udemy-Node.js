ba9aea33a474d2fd5fcad1762ac139a2d4b9b46a
Denis, Makarov <makarovdenis07081985@gmail.com>
2020-12-03 20:34:09 +0200

    get value of form

--- server/express.js
+++ server/express.js
@@ -2,44 +2,63 @@ const express = require('express')
 const path = require('path')
 const exphbs = require('express-handlebars')
 
+// --- экспортируем роуты ---
+const homeRoutes = require('./routs/home')
+const coursesRoutes = require('./routs/courses')
+const addRoutes = require('./routs/add')
+
+
 const app = express()
-// настраиваем движок
+
+// --- настраиваем движок ---
 const hbs = exphbs.create({
     defaultLayout: 'main',
     extname: 'hbs'
 })
-// регистрируем в express
+
+// --- регистрируем движок hbs в express ---
 app.engine('hbs', hbs.engine)
-// использование
+
+// --- использование ---
 app.set('view engine', 'hbs')
-// указания пути шаблонов
+
+// --- указания пути шаблонов ---
 app.set('views', path.join(__dirname, 'views')) // второй параметр название папки с шаблонами
-// регестрируем статические файлы (где будут хранится например css...)
+
+// --- регестрируем статические файлы (где будут хранится например css...) ---
 app.use(express.static(path.join(__dirname, 'public')))
 
-//// --- c использованием handlebars --- ////
-app.get('/', (req, res, next) => {
-    res.render('index', {
-        title: 'Home Page',
-        isHome: true
-    })
-})
+app.use(express.urlencoded({extended: true}))
 
-app.get('/courses',  (req, res, next) => {
-    res.render('courses', {
-        title: "Courses",
-        isCourses: true
-    })
-})
-
-app.get('/add',  (req, res, next) => {
-    res.render('add', {
-        title: "Add Course",
-        isAdd: true
-    })
-})
+// --- регестрируем роуты с префиксами пути---
+app.use('/', homeRoutes)
+app.use('/courses', coursesRoutes)
+app.use('/add', addRoutes)
 
 
+
+// //// --- lesson before registreted routes
+// //// --- c использованием handlebars --- ////
+// app.get('/', (req, res, next) => {
+//     res.render('index', {
+//         title: 'Home Page',
+//         isHome: true
+//     })
+// })
+//
+// app.get('/courses',  (req, res, next) => {
+//     res.render('courses', {
+//         title: "Courses",
+//         isCourses: true
+//     })
+// })
+//
+// app.get('/add',  (req, res, next) => {
+//     res.render('add', {
+//         title: "Add Course",
+//         isAdd: true
+//     })
+// })
 
 
 //// --- без использования handlebars --- ////
