3fa0448c09dfc2716886db212a0243fc25575803
Denis, Makarov <makarovdenis07081985@gmail.com>
2020-12-04 14:22:12 +0200

    create model of product and save it in JSON file

--- server/routs/add.js
+++ server/routs/add.js
@@ -1,16 +1,17 @@
 const {Router} = require('express')
 const router = Router()
+const Course = require('../models/course')
 
-router.get('/',  (req, res, next) => {
+router.get('/', (req, res, next) => {
     res.render('add', {
         title: "Add new course",
         isAdd: true
     })
 })
 
-router.post('/', (req, res) => {
-    console.log(req.body)
-
+router.post('/', async (req, res) => {
+    const course = new Course(req.body.title, req.body.price, req.body.image)
+    await course.save()
     res.redirect('/courses') // после обработки запроса переводит на др. страницу
 })
 module.exports = router
