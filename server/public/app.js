b50a9dcb782a76daec0aea492afab2e42a2969da
Denis, Makarov <makarovdenis07081985@gmail.com>
2020-12-07 15:11:43 +0200

    Lesson: 35 || create html of card

--- server/public/app.js
+++ server/public/app.js
@@ -1,22 +1,44 @@
+
+const toCurrency = price => {
+    return new Intl.NumberFormat('ca-CA', {
+        currency: 'CAD',
+        style: 'currency'
+    }).format(price)
+}
+
 document.querySelectorAll('.price').forEach(el => {
-  el.textContent = new Intl.NumberFormat('ca-CA', {
-      currency: 'CAD',
-      style: 'currency'
-  }).format(el.textContent)
+    el.textContent = toCurrency(el.textContent)
 })
 
 
 const $card = document.querySelector('#card')
-if ($card){
+if ($card) {
     $card.addEventListener('click', event => {
-        if (event.target.classList.contains('js-remove')){
+        if (event.target.classList.contains('js-remove')) {
             const id = event.target.dataset.id
 
-            fetch('/card/remove/'+id, {
+            fetch('/card/remove/' + id, {
                 method: 'delete',
             }).then((res) => res.json())
-                .then(data => console.log(data))
-
+                .then(card => {
+                    if (card.courses.length) {
+                        const html = card.courses.map(el => {
+                            return `
+                                 <tr>
+                                    <td>${el.title}</td>
+                                    <td>${el.count}</td>
+                                    <td>
+                                        <button class="btn btn-small js-remove" data-id="${el.id}">Remove course</button>
+                                    </td>
+                                 </tr>
+                            `
+                        }).join('')
+                        $card.querySelector('tbody').innerHTML = html
+                        $card.querySelector('.price').textContent = toCurrency(card.price)
+                    }else{
+                        $card.innerHTML = "Card is empty"
+                    }
+                })
             console.log(id)
         }
     })
