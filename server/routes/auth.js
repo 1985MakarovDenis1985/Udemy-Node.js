6c39999f405907ababfc2a20637b950ca2d5f1a5
Denis, Makarov <makarovdenis07081985@gmail.com>
2020-12-27 13:05:12 +0200

    Lesson ...59: refistration / login / bcrypt

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,6 @@
 const {Router} = require('express')
 const router = Router()
+const bcrypt = require('bcryptjs') // шифрование пароля
 const User = require('../models/user')
 
 
@@ -17,15 +18,55 @@ router.get('/logout', async (req, res) => {
 })
 
 router.post('/login', async (req, res) => {
-    const user = await User.findById('5fdcb8825c58fffd616a1062')
-    req.session.user = user
-    req.session.isAuthenticated = true
-    req.session.save(err => { // fun для сохранения данных перед переходом
-        if (err){
-            throw err
+    try {
+        const {email, password} = req.body
+        const candidate = await User.findOne({email})
+
+        if (candidate) {
+            const areSame = await bcrypt.compare(password, candidate.password) // bcrypt сравнивает пароли
+            if (areSame) {
+                req.session.user = candidate
+                req.session.isAuthenticated = true
+                req.session.save(err => { // fun для сохранения данных перед переходом
+                    if (err) {
+                        throw err
+                    } else {
+                        res.redirect('/')
+                    }
+                })
+            } else {
+                res.redirect('/auth/login#login')
+            }
         } else {
-            res.redirect('/')
+            res.redirect('/auth/login#login')
         }
-    })
+    } catch (err) {
+        console.log(err)
+    }
 })
+
+router.post('/registration', async (req, res) => {
+    try {
+        const {name, email, password, repeat} = req.body
+        const candidate = await User.findOne({email})
+
+        if (candidate) { // --- проверяем: существует ли уже такой кандидат в базе
+            res.redirect('/auth/login/registration')
+        } else {                                                // await - всегда перед асинхронными функциями
+            const hasPassword = await bcrypt.hash(password, 10) // 1- что шифруем, 2- уровень шифрования
+            const user = new User({
+                name,
+                email,
+                password: hasPassword,
+                cart: {items: []}
+            })
+            await user.save()
+            res.redirect('/auth/login#login')
+        }
+
+    } catch (err) {
+        console.log(err)
+    }
+})
+
 module.exports = router
